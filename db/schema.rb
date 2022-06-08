f9e30e970b12802e86fd05bb215dc08739af3c21
loan <loancrea@gmail.com>
2022-06-08 11:51:55 +0200

    feat(user): add user db / model / view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_07_080015) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_08_094937) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -28,6 +28,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_07_080015) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
