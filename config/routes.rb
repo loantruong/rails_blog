58a773c2ef00994db2e01e3aa23bfcb952bc5481
loan <loancrea@gmail.com>
2022-06-21 16:56:17 +0200

    feat(sessions): add routes and ui for log in

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
   root "articles#index"
   get "signup", to: "users#new"
-
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
   resources :articles do
     resources :comments
   end
