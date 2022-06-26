bca41964daf6604100ab1dd7dc2bc5f112532443
loan <loancrea@gmail.com>
2022-06-26 18:11:47 +0200

    feat(session): add helpers login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def current_user
+    # memorisation -> have current user or ...
+    @current_user || User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user #return boolean
+  end
 end
