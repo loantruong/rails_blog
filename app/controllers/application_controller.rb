a544e984304a262618d26365dcd7038926eeffe7
loan <loancrea@gmail.com>
2022-06-27 21:55:56 +0200

    refactor(helpers): move helpers into application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
-  def current_user
+  ## add to helper method but in application controller access by all controllers
+  helper_method :current_user, :logged_in?
 
+  def current_user
+    # memorisation -> have current user or ...
+    @current_user || User.find(session[:user_id]) if session[:user_id]
   end
+
+  def logged_in?
+    !!current_user #return boolean
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
 module ApplicationHelper
-
-  def current_user
-    # memorisation -> have current user or ...
-    @current_user || User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    !!current_user #return boolean
-  end
 end
