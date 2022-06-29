1053db51be8043aea4b372184761110e3161e418
loan <loancrea@gmail.com>
2022-06-29 12:17:18 +0200

    feat(migration): add admin column to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_15_064503) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_29_095442) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_15_064503) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "comments", "articles"
