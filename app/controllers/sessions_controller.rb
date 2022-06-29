61cb05c1a4c3e208967549944d35816b6433b48f
loan <loancrea@gmail.com>
2022-06-29 10:03:12 +0200

    refactor(clean code)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "Logged out"
-    redirect_to root_path
+    redirect_to root_path, notice: "Logged out"
   end
 end
