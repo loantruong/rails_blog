16f3ee607f68feca3a34a88819b98429ab22499d
loan <loancrea@gmail.com>
2022-06-29 11:51:48 +0200

    fix(redirect): on logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: "Logged out"
+    redirect_to root_path, notice: "Logged out", status: :see_other
   end
 end
