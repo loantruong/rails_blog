1a8929bed63bc0e91c0e674c5f2d36d5924b1f86
loan <loancrea@gmail.com>
2022-06-29 12:17:31 +0200

    feat(admin): ui and controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
   # DELETE /users/1 or /users/1.json
   def destroy
     @user.destroy
-    session[:user_id] = nil
+    session[:user_id] = nil if @user == current_user
     #flash[:notice] = "Account and all associated articles successfully deleted"
     redirect_to root_path, status: :see_other
   end
@@ -72,7 +72,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
       flash[:alert] = "You can only edit your own account"
       redirect_to @user
     end
